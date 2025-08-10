2957b5473e2fa64d3b6d345d1a4bb0e5fe097fab
Nidhicodes <nidhisingh200418@gmail.com>
2025-08-10 08:43:03 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
-
 export default defineConfig({
+  base: '/static/',       
   build: {
-    outDir: 'build'  // output inside frontend/build
+    outDir: 'build'     
   }
 });
